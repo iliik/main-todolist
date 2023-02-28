62522b0616ef52266b69befe768b5075955daa7f
Ilya <koselevicila@gmail.com>
2023-02-28 15:32:13 +0300

    Todolist-11- Todolist-filter

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -66,6 +66,7 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             if (task) {
                 task.isDone = action.isDone;
             }
+            state[action.todolistId] = [...todolistTasks]
             return ({...state});
         }
         case 'CHANGE-TASK-TITLE': {
@@ -76,6 +77,7 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             if (task) {
                 task.title = action.title;
             }
+            state[action.todolistId] = [...todolistTasks]
             return ({...state});
         }
         case 'ADD-TODOLIST': {
