696a90f8645dec66e2a1a3173922812a8215de60
Ilya <koselevicila@gmail.com>
2023-03-01 18:00:38 +0300

    Todolist-14-storybook-api-post-headers

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -5,14 +5,19 @@ export default {
     title: 'API'
 }
 const setting = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': '45e010e4-6515-4f44-948c-de88f0fc6daf'
+    }
 }
 
 export const GetTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        let promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', setting)
-
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', setting)
+            .then((res) => {
+                setState(res.data)
+            })
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -20,7 +25,10 @@ export const GetTodolist = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'ILYA-BATUMI'}, setting)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
     return <div>{JSON.stringify(state)}</div>
 }
