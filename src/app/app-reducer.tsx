c6411446b3b150760cef067fc43ba499cbc210da
Ilya <koselevicila@gmail.com>
2023-03-16 17:06:01 +0300

    Todolist-addTask

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -2,7 +2,13 @@ export type StatusActionsType = {
     type: 'APP/SET-STATUS'
     status: RequestStatusType
 }
+export type ErrorActionsType = {
+    type: 'APP/SET-ERROR'
+    error: null
 
+}
+
+type ActionType = StatusActionsType | ErrorActionsType
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
@@ -12,13 +18,17 @@ const initialState = {
 
 type InitialStateType = typeof initialState
 
-export const appReducer = (state: InitialStateType = initialState, action: StatusActionsType): InitialStateType => {
+export const appReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR': {
+            return {...state, error: action.error}
+        }
         default:
             return state
     }
 }
 
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error: null) => ({type: 'APP/SET-ERROR', error} as const)
