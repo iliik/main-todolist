384c49cf2c127836a670a6e47a2ca861786b1b13
kosel <koselevicila@gmail.com>
2023-03-26 18:41:38 +0300

    formik-validate-teg-form

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -51,32 +51,33 @@ export const Login = () => {
                     <p>Email: free@samuraijs.com</p>
                     <p>Password: free</p>
                 </FormLabel>
-                <form onSubmit={formik.handleChange}>
+
+                    <form onSubmit={formik.handleSubmit}>
                     <FormGroup>
-                        <TextField label="Email"
-                                   margin="normal"
-                            {...formik.getFieldProps('email')}
-                        />
-                        {formik.touched.email && formik.errors.email &&
-                            <div style={{color: " red"}}>{formik.errors.email}</div>}
 
-                        <TextField type="password"
-                                   label="Password"
-                                   margin="normal"
-                                   {...formik.getFieldProps('password')}
-                        />
-                        {formik.touched.password && formik.errors.password &&
-                            <div style={{color: " red"}}>{formik.errors.password}</div>}
-                        <FormControlLabel
-                            label={'Remember me'}
-                            control={<Checkbox
-                                {...formik.getFieldProps('rememberMe')}/>}/>
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
-                            Login
-                        </Button>
+                            <TextField label="Email"
+                                       margin="normal"
+                                       {...formik.getFieldProps('email')}
+                            />
+                            {formik.touched.email && formik.errors.email &&
+                                <div style={{color: " red"}}>{formik.errors.email}</div>}
+
+                            <TextField type="password"
+                                       label="Password"
+                                       margin="normal"
+                                       {...formik.getFieldProps('password')}
+                            />
+                            {formik.touched.password && formik.errors.password &&
+                                <div style={{color: " red"}}>{formik.errors.password}</div>}
+                            <FormControlLabel
+                                label={'Remember me'}
+                                control={<Checkbox
+                                    {...formik.getFieldProps('rememberMe')}/>}/>
+                            <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                Login
+                            </Button>
                     </FormGroup>
-                </form>
-
+                        </form>
             </FormControl>
         </Grid>
     </Grid>
