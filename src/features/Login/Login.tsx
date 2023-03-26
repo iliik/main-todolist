c218aeb1701bd4a02feb33a16b46d6b3d60e3580
kosel <koselevicila@gmail.com>
2023-03-26 18:09:11 +0300

    formik-validate-formik-getProps

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -55,26 +55,22 @@ export const Login = () => {
                     <FormGroup>
                         <TextField label="Email"
                                    margin="normal"
-                                   name={'email'}
-                                   onChange={formik.handleChange}
-                                   value={formik.values.email}
+                            {...formik.getFieldProps('email')}
                         />
-                        {formik.errors.email && <div style={{color: " red"}}>{formik.errors.email}</div>}
+                        {formik.touched.email && formik.errors.email &&
+                            <div style={{color: " red"}}>{formik.errors.email}</div>}
 
                         <TextField type="password"
                                    label="Password"
-                                   name={"Password"}
                                    margin="normal"
-                                   onChange={formik.handleChange}
-                                   value={formik.values.password}
+                                   {...formik.getFieldProps('password')}
                         />
-                        {formik.errors.password && <div style={{color: " red"}}>{formik.errors.password}</div>}
+                        {formik.touched.password && formik.errors.password &&
+                            <div style={{color: " red"}}>{formik.errors.password}</div>}
                         <FormControlLabel
                             label={'Remember me'}
                             control={<Checkbox
-                                name={'rememberMe'}
-                                onChange={formik.handleChange}
-                                value={formik.values.rememberMe}/>}/>
+                                {...formik.getFieldProps('rememberMe')}/>}/>
                         <Button type={'submit'} variant={'contained'} color={'primary'}>
                             Login
                         </Button>
